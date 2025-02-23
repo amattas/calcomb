0b6a1c74cdcb4a84ce77c4dd2a3c77fb2d055f6d
Anthony Mattas <anthony@mattas.net>
2025-02-23 15:08:00 -0500

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -2,15 +2,14 @@ import azure.functions as func
 import logging
 import os
 import json
-import sys
+import requests
 import uuid
 import hashlib
 from pathlib import Path
 from zoneinfo import ZoneInfo
 from datetime import timedelta, datetime, date
-
-import requests
 from icalendar import Calendar, Event
+
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -42,7 +41,8 @@ def get_cal(req: func.HttpRequest) -> func.HttpResponse:
     # Set today's date in UTC using built-in zoneinfo (Python 3.9+)
     today = datetime.now(ZoneInfo("UTC")).date()
 
-
+    # Create the temporary calendar dictionary, we use this to handle duplicate UIDs
+    temp_cal = { }
 
     # Create the combined calendar
     combined_cal = Calendar()
@@ -61,28 +61,25 @@ def get_cal(req: func.HttpRequest) -> func.HttpResponse:
                 response.raise_for_status()
             except requests.RequestException as err:
                 return func.HttpResponse(f"Unable to fetch calendar with id {calendar.get('Id')}", status_code=500)
-                continue
 
             try:
                 ical = Calendar.from_ical(response.text)
             except Exception as err:
                 return func.HttpResponse(f"Unable to parse calendar with id {calendar.get('Id')}", status_code=500)
-                # print(f"Error parsing calendar from {calendar['Url']}: {err}", file=sys.stderr)
-                # continue
 
             for component in ical.walk("VEVENT"):
                 end = component.get("dtend")
 
-                # Only show configured historical events unless recurring.
+                # Only show configured historical events
                 if end and days_history: 
                     dt_val = end.dt
                     if isinstance(dt_val, datetime):
                         event_date = dt_val.date()
                     else:
                         event_date = dt_val
+                    # Filter out older non-recurring
                     if event_date < today - timedelta(days=days_history) and "RRULE" not in component:
                         continue
-
 
                 copied_event = Event()
                 # Copy all properties from the original event
@@ -128,8 +125,20 @@ def get_cal(req: func.HttpRequest) -> func.HttpResponse:
 
                 # Remove Organizer
                 copied_event.pop("ORGANIZER", None)
+                
+                # Remove empty lines from the description, since they aren't technically supported by the ICalendar RFC
+                if copied_event.get("DESCRIPTION"):
+                    copied_event["DESCRIPTION"] = os.linesep.join([s for s in copied_event.get("DESCRIPTION").splitlines() if s])
 
-                combined_cal.add_component(copied_event)
+                temp_cal[copied_event['UID']] = copied_event
+                
+    
+    # Add temp_cal items to combined_cal
+    for e in temp_cal.values():
+        combined_cal.add_component(e)
+
+    # Add missing timezones
+    combined_cal.add_missing_timezones()
 
     # Write the combined calendar to the output file in binary mode.
     return func.HttpResponse(combined_cal.to_ical(),
